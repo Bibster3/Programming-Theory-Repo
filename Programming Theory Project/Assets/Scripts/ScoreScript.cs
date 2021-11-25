35d0ac8e391fbe9f4288910bc0200f7fc5bdb2f4
Bibster3 <bilyana.f.stefanova@gmail.com>
2021-11-25 23:43:10 +0200

    Highscore added, Change of score spheres,  BG sound

--- Programming Theory Project/Assets/Scripts/ScoreScript.cs
+++ Programming Theory Project/Assets/Scripts/ScoreScript.cs
@@ -5,15 +5,29 @@ using UnityEngine.UI;
 
 public class ScoreScript : MonoBehaviour
 {
+    Renderer[] children;
 
+
+    public void Start()
+    {
+    }
     private void OnTriggerEnter(Collider other)
     {
 
 
+        children = other.GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer rend in children)
+        {
+            StartCoroutine(SwitchColor(rend));
+        }
+
         if (other.gameObject.tag == "1")
         {
+            
 
             UIScript.Score = UIScript.Score + 1;
+              
         }
 
         if (other.gameObject.tag == "2")
@@ -24,16 +38,23 @@ public class ScoreScript : MonoBehaviour
 
         if (other.gameObject.tag == "3")
         {
-
             UIScript.Score = UIScript.Score + 3;
         }
 
         if (other.gameObject.tag == "4")
         {
-
             UIScript.Score = UIScript.Score + 4;
         }
         Debug.Log("TriggerHit " + UIScript.Score);
 
     }
+    System.Collections.IEnumerator SwitchColor(Renderer renderer)
+    {
+        Color color = renderer.material.color;
+        renderer.material.color = Color.green;
+        yield return new WaitForSeconds(0.2f);
+        renderer.material.color = Color.white;
+    }
+
+
 }
