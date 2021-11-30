52af3d61254aaac6e3c2df0c8bd4e85816a16ef3
Bibster3 <bilyana.f.stefanova@gmail.com>
2021-11-30 03:12:56 +0200

    work in progress

---
+++ Programming Theory Project/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicManager : MonoBehaviour
+{
+
+    public static int music;
+
+    public static MusicManager instance;
+
+    private void Awake()
+    {
+        music = PlayerPrefs.GetInt("Music");
+
+        gameObject.transform.parent = null;
+        DontDestroyOnLoad(transform.gameObject);
+
+       if (instance == null)
+        {
+           
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            return;
+
+        }
+    }
+
+    void Start()
+    {
+        music = PlayerPrefs.GetInt("Music");
+
+        Debug.Log("music: " + music);
+    }
+
+    public static void SetInt()
+    {
+        PlayerPrefs.SetInt("Music", music);
+    }
+}
